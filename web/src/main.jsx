0fc53510d0c9f52c1453c3291b43c1191c823597
Aashish Loknath Panigrahi <n00bgineer@protonmail.com>
2025-03-28 15:30:01 +0530

    Setting up tailwind

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,9 +1,13 @@
+import App from './App.jsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter } from "react-router";
+import "./index.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
