c97c8f45c433d400eab2cfcd86e98bb7fb985949
Aashish Loknath Panigrahi <n00bgineer@protonmail.com>
2025-03-28 20:49:26 +0530

    Adding schema definition for user & user session

---
+++ api/src/db/schema.js
@@ -0,0 +1,43 @@
+import { pgTable, serial, text, varchar, timestamp, boolean, json, uuid } from 'drizzle-orm/pg-core';
+import { sql, relations } from 'drizzle-orm';
+
+// USER SCHEMA
+export const user = pgTable('user', {
+  id: uuid('id').default(sql`gen_random_uuid()`).primaryKey(),
+  createdAt: timestamp('created_at').default(sql`CURRENT_TIMESTAMP`).notNull(),
+  updatedAt: timestamp('updated_at').default(sql`CURRENT_TIMESTAMP`).notNull(),
+  
+  // USER IDENTIFIERS
+  auth0Id: varchar('auth0_id', { length: 128 }).unique().notNull(),
+  googleId: varchar('google_id', { length: 128 }),
+
+  // USER PROFILE DATA
+  name: text('name').notNull(),
+  email: varchar('email', { length: 255 }).unique().notNull(),
+  emailVerified: boolean('email_verified').default(false),
+  picture: text('picture'),
+  isActive: boolean('is_active').default(true),
+});
+
+// USER RELATIONS
+export const userRelations = relations(user, ({ many }) => ({
+  sessions: many(userSession)
+}))
+
+// USER SESSION DATA
+export const userSession = pgTable('user_sessions', {
+  id: uuid('id').default(sql`gen_random_uuid()`).primaryKey(),
+  createdAt: timestamp('created_at').default(sql`CURRENT_TIMESTAMP`).notNull(),
+  expiresAt: timestamp('expires_at').notNull(),
+
+  userId: serial('user_id').references(() => user.id).notNull(),
+  token: text('token').notNull(),
+});
+
+// USER SESSION RELATIONS
+export const userSessionRelations = relations(userSession, ({ one }) => ({
+  user: one(user, {
+    fields: [userSession.userId],
+    references: [user.id],
+  })
+}));
