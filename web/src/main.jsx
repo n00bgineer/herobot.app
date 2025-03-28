aaee9d10ca5fa745a6a1b3a95ba90a0b34cf3f17
Aashish Loknath Panigrahi <n00bgineer@protonmail.com>
2025-03-28 20:51:08 +0530

    Adding auth0 config to the main wrapper config

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,12 +2,21 @@ import App from './App.jsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
+import { Auth0Provider } from '@auth0/auth0-react';
 import "./index.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <Auth0Provider
+        domain={import.meta.env.VITE_AUTH0_DOMAIN}
+        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+        onRedirectCallback={(appState)=>{console.log(appState)}}
+        cacheLocation="localstorage"
+        useRefreshTokens={true}
+      >
       <App />
+      </Auth0Provider>
     </BrowserRouter>
   </StrictMode>,
 )
