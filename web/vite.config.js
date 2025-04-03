1d3bd51576bea304ab86b77d4c9d337514b5851f
Aashish Loknath Panigrahi <n00bgineer@protonmail.com>
2025-04-03 18:38:15 +0530

    Using alias for 'src'

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,13 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      'src': path.resolve(__dirname, './src')
+    }
+  },
   plugins: [
     react()
   ],
