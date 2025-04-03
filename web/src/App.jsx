35dc8314ea61598f7588551ec60e0373dee7a812
Aashish Loknath Panigrahi <n00bgineer@protonmail.com>
2025-04-03 18:38:03 +0530

    Updating page names

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,38 +1,38 @@
 import { Routes, Route } from 'react-router'
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
-import ToS from './pages/ToS/ToS'
-import Privacy from './pages/Privacy/Privacy'
 import AppLayout from './layouts/AppLayout/AppLayout'
-import Roadmap from './pages/Roadmap/Roadmap/Roadmap'
-import Roadmaps from './pages/Roadmap/Roadmaps/Roadmaps'
-import EditRoadmap from './pages/Roadmap/EditRoadmap/EditRoadmap'
-import CreateRoadmap from './pages/Roadmap/CreateRoadmap/CreateRoadmap'
-import Tokens from './pages/Token/Tokens/Tokens'
-import CreateToken from './pages/Token/CreateToken/CreateToken'
+import HomePage from './pages/HomePage/HomePage'
+import ToSPage from './pages/ToSPage/ToSPage'
+import PrivacyPage from './pages/PrivacyPage/PrivacyPage'
+import AboutPage from './pages/AboutPage/AboutPage'
+import RoadmapsPage from './pages/Roadmap/RoadmapsPage/RoadmapsPage'
+import RoadmapPage from './pages/Roadmap/RoadmapPage/RoadmapPage'
+import CreateRoadmapPage from './pages/Roadmap/CreateRoadmapPage/CreateRoadmapPage'
+import EditRoadmapPage from './pages/Roadmap/EditRoadmapPage/EditRoadmapPage'
+import TokensPage from './pages/TokensPage/TokensPage'
+import CreateTokenPage from './pages/CreateTokenPage/CreateTokenPage'
 
 function App() {
   return (
     <Routes>
         {/* GENERAL ROUTES */}
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path='tos' element={<ToS />} />
-      <Route path="privacy" element={<Privacy />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="about" element={<AboutPage />} />
+      <Route path='terms' element={<ToSPage />} />
+      <Route path="privacy" element={<PrivacyPage />} />
 
       <Route element={AppLayout}>
         {/* ROADMAP ROUTES */}
-        <Route path='roadmaps' element={<Roadmaps />} />
+        <Route path='roadmaps' element={<RoadmapsPage />} />
         <Route path="/roadmap">
-          <Route path=":id" element={<Roadmap />} />
-          <Route path="create" element={<CreateRoadmap />} />
-          <Route path="edit/:id" element={<EditRoadmap />} />
+          <Route path=":id" element={<RoadmapPage />} />
+          <Route path="create" element={<CreateRoadmapPage />} />
+          <Route path="edit/:id" element={<EditRoadmapPage />} />
         </Route>
         
         {/* TOKEN ROUTES */}
-        <Route path='tokens' element={<Tokens />} />
+        <Route path='tokens' element={<TokensPage />} />
         <Route path='token'>
-          <Route path="create" element={<CreateToken />} />
+          <Route path="create" element={<CreateTokenPage />} />
         </Route>
       </Route>
 
