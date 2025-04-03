21d39cb908776e42edc69454e00022fbdfc3c0a6
Aashish Loknath Panigrahi <n00bgineer@protonmail.com>
2025-04-03 14:25:47 +0530

    Minor testing

--- api/env.js
+++ api/env.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
-console.log(process.env)
+console.log(process.env["NODE_ENV"])
+console.log(process.env["DATABASE_URL"])
 
 export default {
   NODE_ENV: process.env.NODE_ENV || 'LOCAL',
