00f0587b4441ba24865f1364eb8edb164e378c5a
Aashish Loknath Panigrahi <n00bgineer@protonmail.com>
2025-04-04 16:27:41 +0530

    Adding client URL in the env for cors middleware

--- api/env.js
+++ api/env.js
@@ -3,6 +3,7 @@ import { env } from "cloudflare:workers"
 export default {
     NODE_ENV: env.NODE_ENV || 'LOCAL',
     DATABASE_URL: env.DATABASE_URL,
+    CLIENT_URL: env.CLIENT_URL,
     ATHENA_AGENT_URL: env.ATHENA_AGENT_URL,
     APOLLO_AGENT_URL: env.APOLLO_AGENT_URL,
     HERMES_AGENT_URL: env.HERMES_AGENT_URL,
