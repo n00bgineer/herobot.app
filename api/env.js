ba23528b7939211bba5491364a816276945269a0
Aashish Loknath Panigrahi <n00bgineer@protonmail.com>
2025-04-04 16:15:17 +0530

    Adding env for agent URLs

--- api/env.js
+++ api/env.js
@@ -2,5 +2,8 @@ import { env } from "cloudflare:workers"
 
 export default {
     NODE_ENV: env.NODE_ENV || 'LOCAL',
-    DATABASE_URL: env.DATABASE_URL
+    DATABASE_URL: env.DATABASE_URL,
+    ATHENA_AGENT_URL: env.ATHENA_AGENT_URL,
+    APOLLO_AGENT_URL: env.APOLLO_AGENT_URL,
+    HERMES_AGENT_URL: env.HERMES_AGENT_URL,
 };
