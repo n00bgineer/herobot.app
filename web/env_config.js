c5b83aaa60f7891870caec049c8d7fa1101fa21b
Aashish Loknath Panigrahi <n00bgineer@protonmail.com>
2025-04-06 05:28:32 +0530

    Replacing values with env_config

---
+++ web/env_config.js
@@ -0,0 +1,6 @@
+export default {
+  AUTH_DOMAIN: import.meta.env.VITE_AUTH0_DOMAIN,
+  AUTH_CLIENT_ID: import.meta.env.VITE_AUTH0_CLIENT_ID,
+  AUTH_CALLBACK_URI: import.meta.env.VITE_AUTH0_CALLBACK_URI,
+  API_URL: import.meta.env.VITE_API_URL,
+}
--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,20 +1,23 @@
+import "./index.css"
+import App from './App.jsx'
+import env from '../env_config.js'
+import LightTheme from './themes/lightTheme.js';
+import { HelmetProvider } from 'react-helmet-async';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
 import { Auth0Provider } from '@auth0/auth0-react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import LightTheme from './themes/lightTheme.js';
-import "./index.css"
-import App from './App.jsx'
-import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Auth0Provider
-        domain={import.meta.env.VITE_AUTH0_DOMAIN}
-        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-        onRedirectCallback={(appState)=>{console.log(appState)}}
+        domain={env.AUTH_DOMAIN}
+        clientId={env.AUTH_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: env.AUTH_CALLBACK_URI,
+        }}
         cacheLocation="localstorage"
         useRefreshTokens={true}
       >
