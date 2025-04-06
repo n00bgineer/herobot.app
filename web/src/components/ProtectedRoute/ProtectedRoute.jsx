b0683480c8baceeed120abd6c4c1d684ff71cf05
Aashish Loknath Panigrahi <n00bgineer@protonmail.com>
2025-04-06 20:16:02 +0530

    Returning splash screen from protected route

--- web/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ web/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,12 +1,13 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate } from "react-router";
+import SplashScreen from "../SplashScreen/SplashScreen";
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, isLoading } = useAuth0();
 
   // IN LOADING STATE
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <SplashScreen showProgress={true} />;
   }
   else{
     // IN UN-AUTHENTICATED STATE
