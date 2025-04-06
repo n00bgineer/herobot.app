9191f5288e83180d0c9b371f023ec5b40e8756a1
Aashish Loknath Panigrahi <n00bgineer@protonmail.com>
2025-04-06 20:25:50 +0530

    Adding text with logo

--- web/src/components/SplashScreen/SplashScreen.jsx
+++ web/src/components/SplashScreen/SplashScreen.jsx
@@ -1,10 +1,12 @@
 // IMPORTING PACKAGES/MODULES
 import { useEffect, useState } from 'react'
-import { Box, Typography, } from '@mui/material'
+import { Box, LinearProgress, Typography, } from '@mui/material'
 import { useAuth0 } from '@auth0/auth0-react'
 import useStore from '../../state/store'
 import AppLogoOptimised from 'src/assets/herosite-logo--optimised.webp'
 import AppLogo from 'src/assets/herosite-logo.webp'
+import SplashScreenContainer from './SplashScreenContainer'
+import CustomImage from '../Custom/CustomImage/CustomImage'
 
 const SplashScreen = ({ showProgress = false }) => {
   // SETTING LOCAL STATES
@@ -38,11 +40,17 @@ const SplashScreen = ({ showProgress = false }) => {
   return (
     <SplashScreenContainer className="splash-modal">
       <Box className="splash-modal-content-container">
-        <CustomImage
-          src={AppLogo}
-          placeholderSrc={AppLogoOptimised}
-          className="app-logo-large"
-          alt="App Logo" />
+        <Box className="app-logo-container">
+          <CustomImage
+            src={AppLogo}
+            placeholderSrc={AppLogoOptimised}
+            alt="App logo"
+            className="app-logo-medium"
+          />
+          <Typography variant="h3" className="app-name">
+            HeroBot
+          </Typography>
+        </Box>
         {
           showProgress &&
           <>
