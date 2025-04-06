9b3fd8bf19e1475df25e6bc2565aca9feb00427b
Aashish Loknath Panigrahi <n00bgineer@protonmail.com>
2025-04-06 19:22:36 +0530

    Renaming variable to auth

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,11 +2,11 @@ import { Hono } from "hono";
 import { sendSuccess } from "../utils/responseHandler";
 import { authMiddleware } from "../middleware/auth";
 
-const app = new Hono().basePath('/auth');
+const auth = new Hono().basePath('/auth');
 
-app.get('/', authMiddleware , async (ctx) => {
+auth.get('/', authMiddleware , async (ctx) => {
   const userData = ctx.get("user");
   return sendSuccess(ctx, userData);
 })
 
-export default app;
+export default auth;
