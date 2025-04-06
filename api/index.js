3f80acfcb8c8d868f1077f806c31afebd5e75eec
Aashish Loknath Panigrahi <n00bgineer@protonmail.com>
2025-04-06 19:15:55 +0530

    Adding auth route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import { cors } from 'hono/cors'
 import { logger } from 'hono/logger'
 import agent from './routes/agent'
 import { sendSuccess } from './utils/responseHandler'
+import auth from './routes/auth'
 
 // GETTING CORS ALLOWED ORIGIN
 const agentOrigins = [
@@ -39,5 +40,6 @@ app.get('/health', (ctx) => {
   return sendSuccess(ctx, null, "ONLINE")
 });
 app.route('/', agent);
+app.route('/', auth);
 
 export default app;
