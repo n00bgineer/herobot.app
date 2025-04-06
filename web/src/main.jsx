0df6289775d9e6096bc07204e136f63abff1c937
Aashish Loknath Panigrahi <n00bgineer@protonmail.com>
2025-04-06 11:08:53 +0530

    Adding fallback param to make it work

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,6 +1,6 @@
 import "./index.css"
 import App from './App.jsx'
-import env from '../env_config.js'
+import env_config from '../env_config.js'
 import LightTheme from './themes/lightTheme.js';
 import { HelmetProvider } from 'react-helmet-async';
 import { StrictMode } from 'react'
@@ -13,13 +13,16 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Auth0Provider
-        domain={env.AUTH_DOMAIN}
-        clientId={env.AUTH_CLIENT_ID}
+        domain={env_config.AUTH0_DOMAIN}
+        clientId={env_config.AUTH0_CLIENT_ID}
         authorizationParams={{
-          redirect_uri: env.AUTH_CALLBACK_URI,
+          audience: env_config.AUTH0_AUDIENCE,
+          redirect_uri: env_config.AUTH0_REDIRECT_URI,
+          scope: 'openid profile email offline_access',
         }}
         cacheLocation="localstorage"
         useRefreshTokens={true}
+        useRefreshTokensFallback={true}
       >
       <ThemeProvider theme={LightTheme}>
         <CssBaseline />
